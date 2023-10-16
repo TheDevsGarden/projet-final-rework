0667c8469dcbe3d6657d4936dc9ce191a5bd5134
TheDevsGarden <iteratebelyaev@gmail.com>
2023-10-15 22:01:39 -0400

    :bug: with slides, working but ugly

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -45,6 +45,7 @@
 
       <ion-label><h2>I WOULD LIKE TO COOK</h2></ion-label>
       <swiper-container
+        class="wider-slide"
         :slides-per-view="3"
         :space-between="spaceBetween"
         :centered-slides="true"
@@ -59,9 +60,36 @@
         @progress="onProgress"
         @slidechange="onSlideChange"
       >
-        <swiper-slide>Slide 1</swiper-slide>
-        <swiper-slide>Slide 2</swiper-slide>
-        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>
+          <ion-card color="secondary">
+            <img alt="soup" src="https://shorturl.at/hFIM5" />
+            <ion-card-header>
+              <ion-card-title>Soup</ion-card-title>
+              <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+            </ion-card-header>
+            <ion-card-content> Card Content </ion-card-content>
+          </ion-card>
+        </swiper-slide>
+        <swiper-slide>
+          <ion-card color="secondary">
+            <img alt="soup" src="https://shorturl.at/hFIM5" />
+            <ion-card-header>
+              <ion-card-title>Soup</ion-card-title>
+              <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+            </ion-card-header>
+            <ion-card-content> Card Content </ion-card-content>
+          </ion-card>
+        </swiper-slide>
+        <swiper-slide>
+          <ion-card color="secondary">
+            <img alt="soup" src="https://shorturl.at/hFIM5" />
+            <ion-card-header>
+              <ion-card-title>Soup</ion-card-title>
+              <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+            </ion-card-header>
+            <ion-card-content> Card Content </ion-card-content>
+          </ion-card>
+        </swiper-slide>
       </swiper-container>
     </ion-content>
     <ion-footer>Footer</ion-footer>
@@ -75,15 +103,21 @@ import { readerOutline } from "ionicons/icons";
 import { register } from "swiper/element/bundle";
 register();
 
-const spaceBetween = 100;
+const spaceBetween = 1;
 const onProgress = (e) => {
   const [swiper, progress] = e.detail;
   console.log(progress);
 };
-
-const onSlideChange = (e) => {
-  console.log("slide changed");
-};
 </script>
 
-<style scoped></style>
+<style scoped>
+.wider-slide > swiper-slide {
+  min-width: 50%;
+  /* min-height: 50%; */
+}
+.wider-slide > swiper-slide > ion-card > img {
+  width: 100%;
+  height: 50%;
+  object-fit: contain;
+}
+</style>
