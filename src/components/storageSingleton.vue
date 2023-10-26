e6e5e0d92b58411726af758616c6533c168f7cb0
TheDevsGarden <iteratebelyaev@gmail.com>
2023-10-25 20:40:04 -0400

    added storage singletn

---
+++ src/components/storageSingleton.vue
@@ -0,0 +1,23 @@
+<template></template>
+
+<script lang="ts">
+import { Storage } from "@ionic/storage";
+
+let storage: Storage;
+let lastFetchDate: Date;
+
+const Initialize = () => {
+  if (!storage) {
+    createStorage();
+  }
+};
+const createStorage = () => {
+  storage = new Storage();
+  storage.create();
+  lastFetchDate = new Date(0);
+};
+
+export { Initialize };
+</script>
+
+<style scoped></style>
